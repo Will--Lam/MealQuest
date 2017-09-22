9165f3e0d56161dc39f7862ae4d9989cee96f8e1
Will Lam <w23lam@edu.uwaterloo.ca>
2017-09-22 14:09:47 -0400

    Cleaned up search and seach w/ pantry front end

--- MealQuest/MealQuest/SearchWPantryTableViewController.swift
+++ MealQuest/MealQuest/SearchWPantryTableViewController.swift
@@ -14,7 +14,7 @@ class SearchWPantryTableViewController: UIViewController, UITableViewDataSource,
     @IBOutlet weak var searchWPantryButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    var itemArray = [PantryItem]()
+    var pantryItemDic: [String: [PantryItem]] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +35,7 @@ class SearchWPantryTableViewController: UIViewController, UITableViewDataSource,
         let cellNib = UINib(nibName: "PantryArchiveCell", bundle: nil)
         itemTable.register(cellNib, forCellReuseIdentifier: "PantryArchiveCell")
         
-        itemArray = getUniqueActivePantryItems()
+        getUniqueActivePantryItems()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -43,47 +43,58 @@ class SearchWPantryTableViewController: UIViewController, UITableViewDataSource,
     }
     
     // get unique list of pantry items (base case-insensitive name)
-    func getUniqueActivePantryItems() -> [PantryItem] {
-        let items = SQLiteDB.instance.getAllPantryItems()
-        return Array(Set(items))
+    func getUniqueActivePantryItems() {
+        for i in 0...(Constants.pantrySearchGroups.count - 1) {
+            pantryItemDic[Constants.pantrySearchGroups[i]] = getGroupPantryItem(pantryGroup: Constants.pantrySearchGroups[i])
+        }
     }
 
     func refreshData() {
-        itemArray = getUniqueActivePantryItems()
+        getUniqueActivePantryItems()
         itemTable.reloadData()
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return Constants.pantrySearchGroups.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section < Constants.pantrySearchGroups.count {
+            return Constants.pantrySearchGroups[section]
+        }
+        return nil
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return itemArray.count
+        return pantryItemDic[Constants.pantrySearchGroups[section]]!.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PantryArchiveCell", for: indexPath) as! PantryArchiveCell
 
-        cell.itemName.text = itemArray[indexPath.item].name
-        if (itemArray[indexPath.item].search == 0) {
+        cell.itemName.text = pantryItemDic[Constants.pantrySearchGroups[indexPath.section]]?[indexPath.item].name
+        if (pantryItemDic[Constants.pantrySearchGroups[indexPath.section]]?[indexPath.item].search == 0) {
             cell.checkBox.setImage(UIImage(named: "uncheckedBox"), for: .normal)
         } else {
             cell.checkBox.setImage(UIImage(named: "checkedBox"), for: .normal)
         }
         
-        let date = itemArray[indexPath.item].expiration
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = DateFormatter.Style.medium
-        dateFormatter.timeStyle = DateFormatter.Style.none
-        cell.itemExpire.text = "Expiring on " + dateFormatter.string(from: date)
-        
-        cell.groupImage.image = UIImage(named: itemArray[indexPath.item].group)
+        let date = pantryItemDic[Constants.pantrySearchGroups[indexPath.section]]?[indexPath.item].expiration
+        let dateText = getRelativeDate(date: date!)
+        let last2 = dateText.substring(from:dateText.index(dateText.endIndex, offsetBy: -2))
+        if (last2 == "d.") {
+            cell.itemExpire.textColor = UIColor.red
+        } else {
+            cell.itemExpire.textColor = UIColor.gray
+        }
+        cell.itemExpire.text = dateText
+        cell.groupImage.image = UIImage(named: Constants.pantryIconMap[(pantryItemDic[Constants.pantrySearchGroups[indexPath.section]]?[indexPath.item].group)!]!)
         
         cell.search = true
         cell.searchObserver = self
-        cell.pantryItem = itemArray[indexPath.item]
+        cell.pantryItem = pantryItemDic[Constants.pantrySearchGroups[indexPath.section]]?[indexPath.item]
 
         return cell
     }
